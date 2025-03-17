d2bd22122b1925d9c0ebcb0e25dd7f524e9362ec
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-03-17 11:27:55 +0530

    add imageprocessing routes

--- server/src/routes/image.route.ts
+++ server/src/routes/image.route.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { uploadImage, transformImage } from "../controllers/image.controller";
+import { uploadImage, transformImage, getImageById, getTransformedImages, destroyImage, getAllImages } from "../controllers/image.controller";
 import { upload } from "../config";
 import { authMiddleware } from "../middleware/auth.middleware";
 
@@ -8,6 +8,14 @@ const router = Router();
 
 router.route("/").post(authMiddleware, upload.single("file"), uploadImage);
 
+router.route("/").get(authMiddleware, getAllImages);
+
 router.route("/:id/transform").post(authMiddleware, transformImage);
+
+router.route("/:id").get(authMiddleware, getImageById);
+
+router.route("/:id/transforms").get(authMiddleware, getTransformedImages);
+
+router.route("/:id").delete(authMiddleware, destroyImage);
 
 export const imageRouter = router;
