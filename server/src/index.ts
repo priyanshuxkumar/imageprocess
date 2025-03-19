301a5816fcaa586b148dfc074604eadec1771b14
Priyanshu Kumar <priyanshuknp444@gmail.com>
2025-03-19 16:08:47 +0530

    add cors config

--- server/src/index.ts
+++ server/src/index.ts
@@ -10,7 +10,12 @@ const PORT = process.env.PORT || 8000;
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+    {
+        origin: "http://localhost:3000",
+        credentials: true,
+    }
+));
 
 imageProcessingWorker();
 
